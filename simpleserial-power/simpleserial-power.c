b99dc3bcf46b7fd7937de7485f3124f75fc3c37a
Gianluca Pacchiella <gp@ktln2.org>
2021-06-05 18:51:59 +0200

    More experiments.
    Also added traces.

--- simpleserial-power/simpleserial-power.c
+++ simpleserial-power/simpleserial-power.c
@@ -16,6 +16,10 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+#define TTOSTRING(x) TOSTRING(x)
+
 #include "hal.h"
 #include <stdint.h>
 #include <stdlib.h>
@@ -38,10 +42,39 @@ int main(void)
 	*/
 		
     _delay_ms(5000);
+#ifdef SET_ZERO
+    asm volatile(
+    "eor r0, r0" "\n\t"
+    ::
+    );
+#endif
+
+#ifdef SET_FF
+    asm volatile(
+    "ldi r16, 0xff" "\n\t"
+    "mov r0, r16" "\n\t"
+    ::
+    );
+#endif
 
     trigger_high();
     //_delay_ms(10);
 
+#ifdef MUL
+    asm volatile(
+    "mul r0,r1" "\n\t"
+    "mul r0,r1" "\n\t"
+    "mul r0,r1" "\n\t"
+    "mul r0,r1" "\n\t"
+    "mul r0,r1" "\n\t"
+    "mul r0,r1" "\n\t"
+    "mul r0,r1" "\n\t"
+    "mul r0,r1" "\n\t"          
+    "mul r0,r1" "\n\t"
+    "mul r0,r1" "\n\t"
+    ::
+    );
+#endif
 #ifdef NOP
 
     asm volatile(
@@ -58,21 +91,60 @@ int main(void)
     ::
     );
 #endif
-#ifdef MUL
+#ifdef LDI
     asm volatile(
-    "mul r0,r1" "\n\t"
-    "mul r0,r1" "\n\t"
-    "mul r0,r1" "\n\t"
-    "mul r0,r1" "\n\t"
-    "mul r0,r1" "\n\t"
-    "mul r0,r1" "\n\t"
-    "mul r0,r1" "\n\t"
-    "mul r0,r1" "\n\t"          
-    "mul r0,r1" "\n\t"
-    "mul r0,r1" "\n\t"
+#ifdef LDI_AFTER_JMP
+    "rjmp ." TOSTRING(LDI_AFTER_JMP)"\n\t"
+#endif
+    "ldi r16, " TOSTRING(LDI) "\n\t"
+    //"mov r0, r16" "\n\t"
+    ::
+    );
+
+    asm volatile(
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
     ::
     );
 #endif
+
+#if defined SUM_A && defined SUM_B
+    asm volatile(
+    "ldi r17, " TOSTRING(SUM_A) "\n\t"
+    "ldi r16, " TOSTRING(SUM_B) "\n\t"
+#if defined ADC_AFTER_JMP
+    "rjmp ." TOSTRING(ADC_AFTER_JMP) "\n\t"
+#endif
+    "adc r16, r17" "\n\t"
+#ifdef FINAL_MOV
+    "mov r7, r16" "\n\t"
+#endif
+    ::
+    );
+
+    asm volatile(
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    "nop"       "\n\t"
+    ::
+    );
+#endif
+
 #ifdef MUL2
     asm volatile(
     "mul r0,r1" "\n\t"
